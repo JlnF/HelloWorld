e94479729513d01d67f98b14b652d9c1cc50473f
JlnF <62119255+JlnF@users.noreply.github.com>
2023-04-03 20:28:04 +0000

    commit ok

--- Hello.py
+++ Hello.py
@@ -1,8 +1,15 @@
-import time
+import time as time
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 print(time.time())
 
-plt.plot(range(13))
+a = []
+b = []
+for i in range(10):
+    a.append(i**2)
+    b.append(2*i)
+
+plt.plot(a)
+plt.plot(b)
 plt.savefig("test.png")
